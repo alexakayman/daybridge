fa738812f9f508cd258746d59780b455340c51c1
Alexa Kayman <alexakayman@gmail.com>
2024-07-01 09:21:53 -0400

    clear old auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
 
 export default async function Index() {
@@ -17,20 +13,6 @@ export default async function Index() {
       <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
         <Header />
       </div>
-
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>
-          Powered by{" "}
-          <a
-            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Supabase
-          </a>
-        </p>
-      </footer>
     </div>
   );
 }
