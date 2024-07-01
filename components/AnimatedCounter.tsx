aefa0608e64825cf5023c741705e5ad979e8e2db
Alexa Kayman <alexakayman@gmail.com>
2024-06-30 23:27:59 -0400

    counter formatting

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -6,7 +6,7 @@ import CountUp from "react-countup";
 const AnimatedCounter = ({ amount }: { amount: number }) => {
   return (
     <div className="w-full">
-      <CountUp end={amount} prefix="$" decimal="," duration={1} decimals={2} />
+      <CountUp end={amount} prefix="$" decimal="." duration={1} decimals={2} />
     </div>
   );
 };
